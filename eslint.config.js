f9be896e11ef892b52d8f2dd0eb8825cfa8be0d6
edwintantawi <edwintantawi.dev@gmail.com>
2025-06-24 13:36:34 +0700

    feat(eslint): ignore all path on gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 /**
  * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files#configuration-file}
  * @type {import('eslint').Linter.Config}
  */
-const eslintConfig = [];
+const eslintConfig = [
+    includeIgnoreFile(gitignorePath),
+];
 
 export default eslintConfig;
