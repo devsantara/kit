ce5a0b76b7d13ecb7180012a62c98b206f61d3c2
edwintantawi <edwintantawi.dev@gmail.com>
2025-06-24 13:28:15 +0700

    chore(eslint): setup eslint with inspector

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+
+/**
+ * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files#configuration-file}
+ * @type {import('eslint').Linter.Config}
+ */
+const eslintConfig = [];
+
+export default eslintConfig;
