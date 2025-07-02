2fc0854f9c50071efd196873aae5d9264175658d
edwintantawi <edwintantawi.dev@gmail.com>
2025-07-02 23:28:59 +0700

    feat(ui): add geist sans and mono fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,19 @@ import '~/ui/styles/globals.css';
 
 import * as React from 'react';
 
+import { fonts } from '~/ui/styles/fonts';
+import { cn } from '~/ui/utils';
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html
+      className={cn(fonts.sans.variable, fonts.mono.variable, 'antialiased')}
+      lang="en"
+    >
       <body>{children}</body>
     </html>
   );
