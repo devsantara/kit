dc9be7ceb8b0478334cb7bc3a3fa878e766d8f08
edwintantawi <edwintantawi.dev@gmail.com>
2025-12-13 14:00:40 +0700

    style: adjust tanstack router devtools name casing

--- src/devtools/router-devtools.tsx
+++ src/devtools/router-devtools.tsx
@@ -3,6 +3,6 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import type { TanStackDevtoolsReactPlugin } from '@tanstack/react-devtools';
 
 export const tanstackRouterDevtools: TanStackDevtoolsReactPlugin = {
-  name: 'Tanstack Router',
+  name: 'TanStack Router',
   render: <TanStackRouterDevtoolsPanel />,
 };
