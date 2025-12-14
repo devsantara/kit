546f003e09bd09587f3400788f28b3d7ab1daad5
edwintantawi <edwintantawi.dev@gmail.com>
2025-12-14 14:53:21 +0700

    feat(ui): remove unused css important

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ function HomePage() {
   return (
     <main className="grid h-dvh place-content-center p-4">
       <header className="max-w-xl text-center">
-        <h1 className="mb-px text-2xl font-bold!">Devsantara Kit</h1>
+        <h1 className="mb-px text-2xl font-bold">Devsantara Kit</h1>
         <p className="text-neutral-500">The blueprint for your next big idea</p>
       </header>
     </main>
