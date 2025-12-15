624adda7e8c02dd5ca3ae28bc0d2cedb08966d56
edwintantawi <edwintantawi.dev@gmail.com>
2025-12-15 16:35:48 +0700

    refactor(ui): adjust tailwindcss classes

--- src/ui/components/core/drawer.tsx
+++ src/ui/components/core/drawer.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import * as React from 'react';
 import { Drawer as DrawerPrimitive } from 'vaul';
 
@@ -65,7 +63,7 @@ function DrawerContent({
         )}
         {...props}
       >
-        <div className="mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
+        <div className="mx-auto mt-4 hidden h-2 w-25 shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
         {children}
       </DrawerPrimitive.Content>
     </DrawerPortal>
