762a13ddd3922a3df0ced75a6c54c40745fa80b2
edwintantawi <edwintantawi.dev@gmail.com>
2025-12-16 00:32:26 +0700

    refactor: we must import icons with icon suffix

--- src/ui/components/core/carousel.tsx
+++ src/ui/components/core/carousel.tsx
@@ -1,7 +1,7 @@
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from 'embla-carousel-react';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
+import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { Button } from '~/ui/components/core/button';
@@ -194,7 +194,7 @@ function CarouselPrevious({
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft />
+      <ArrowLeftIcon />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -224,7 +224,7 @@ function CarouselNext({
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight />
+      <ArrowRightIcon />
       <span className="sr-only">Next slide</span>
     </Button>
   );
