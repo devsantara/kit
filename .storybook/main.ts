8665c2148e8a2597e15e34610a92319b74e1e564
edwintantawi <edwintantawi.dev@gmail.com>
2025-12-16 15:37:36 +0700

    feat: add public directory for static assets

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import { defineMain } from '@storybook/react-vite/node';
 
 export default defineMain({
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
+  staticDirs: ['../public'],
   framework: '@storybook/react-vite',
   addons: ['@storybook/addon-docs'],
 });
