65a1f905f7515a180f67c144234460d2faa5f0c3
Edwin Tantawi <edwintantawi.dev@gmail.com>
2025-12-19 21:02:57 +0700

    feat(ui): add custom sans and mono fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,8 +8,15 @@ function HomePage() {
   return (
     <main className="grid h-dvh place-content-center p-4">
       <header className="max-w-xl text-center">
-        <h1 className="mb-px text-2xl font-bold">Devsantara Kit</h1>
-        <p className="text-neutral-500">The blueprint for your next big idea</p>
+        <h1 className="mb-px font-sans text-2xl font-extrabold">
+          Devsantara Kit
+        </h1>
+        <p className="font-sans text-sm text-neutral-500">
+          The blueprint for your next big idea
+        </p>
+        <code className="mt-4 inline-block rounded-md border bg-muted px-2 py-1 font-mono text-sm text-muted-foreground">
+          git@github.com:devsantara/kit.git
+        </code>
       </header>
     </main>
   );
