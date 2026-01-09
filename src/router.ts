e16e8400a9c9929cdcb7d8c127b752c71382ad14
Edwin Tantawi <edwintantawi.dev@gmail.com>
2026-01-09 08:16:15 +0700

    fix(posthog): missing defaultErrorComponent cause defaultOnCatch not run

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import { createRouter as createTanstackRouter } from '@tanstack/react-router';
 import { posthog } from 'posthog-js';
 
 import { routeTree } from '~/routeTree.gen';
+import { DefaultErrorPage } from '~/ui/components/default-error-page';
 
 export function getRouter() {
   const router = createTanstackRouter({
     routeTree,
     defaultPreload: 'intent',
     scrollRestoration: true,
+    defaultErrorComponent: DefaultErrorPage,
     defaultOnCatch(error) {
       posthog.captureException(error);
     },
