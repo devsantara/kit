2bbde5c3bf1855d47b9666bde61147a0bd5727a1
Edwin Tantawi <edwintantawi.dev@gmail.com>
2026-01-11 17:04:35 +0700

    feat: use router default error component for now

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,17 @@
-import { createRouter as createTanstackRouter } from '@tanstack/react-router';
+import {
+  createRouter as createTanstackRouter,
+  ErrorComponent,
+} from '@tanstack/react-router';
 import { posthog } from 'posthog-js';
 
 import { routeTree } from '~/routeTree.gen';
-import { DefaultErrorPage } from '~/ui/components/default-error-page';
 
 export function getRouter() {
   const router = createTanstackRouter({
     routeTree,
     defaultPreload: 'intent',
     scrollRestoration: true,
-    defaultErrorComponent: DefaultErrorPage,
+    defaultErrorComponent: ErrorComponent,
     defaultOnCatch(error) {
       posthog.captureException(error);
     },
