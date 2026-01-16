5c0e864792972ba09239d8408365a48d7bf74f6d
Edwin Tantawi <edwintantawi.dev@gmail.com>
2026-01-16 19:01:52 +0700

    feat(seo): add metadata, opengraph, web manifest and icons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,18 +24,20 @@ import { ThemeProvider } from '~/ui/styles/theme';
 
 export const Route = createRootRoute({
   loader: ({ location }) => {
-    return { currentHref: location.url.href };
+    return { currentHref: location.url.origin + location.url.pathname };
   },
   head: ({ loaderData }) => {
     /** @example http://localhost:3000/path/without/locale */
     const currentHref = loaderData?.currentHref ?? '';
 
-    return new HeadBuilder()
+    return new HeadBuilder(new URL(currentHref))
       .addCharSet('utf-8')
       .addViewport({ width: 'device-width', initialScale: 1 })
+      .addStylesheets([{ href: fontStylesheet }, { href: appStylesheet }])
       .addTitle(m.app_name())
       .addDescription(m.app_description())
-      .addStylesheets([{ href: fontStylesheet }, { href: appStylesheet }])
+      .addColorScheme('light dark')
+      .addManifest('/site.webmanifest')
       .addCanonical(localizeHref(currentHref))
       .addAlternateLocales<Locale>({
         'x-default': localizeHref(currentHref, { locale: baseLocale }),
@@ -43,6 +45,35 @@ export const Route = createRootRoute({
         id: localizeHref(currentHref, { locale: 'id' }),
         'zh-CN': localizeHref(currentHref, { locale: 'zh-CN' }),
       })
+      .addIcons({
+        shortcut: [{ url: '/favicon.ico', sizes: '48x48' }],
+        icon: [
+          { type: 'image/svg+xml', url: '/favicon.svg', sizes: 'any' },
+          { type: 'image/png', url: '/favicon-96x96.png', sizes: '96x96' },
+        ],
+        apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+      })
+      .addOpenGraph({
+        title: m.app_name(),
+        description: m.app_description(),
+        locale: getLocale(),
+        type: { name: 'website' },
+        url: localizeHref(currentHref),
+        image: {
+          url: 'https://assets.devsantara.com/kit/thumbnail.jpg',
+          type: 'image/jpeg',
+          width: 1280,
+          height: 640,
+        },
+      })
+      .addTwitter({
+        card: { name: 'summary' },
+        title: m.app_name(),
+        description: m.app_description(),
+        site: '@devsantara_hq',
+        image: { url: 'https://assets.devsantara.com/kit/thumbnail.jpg' },
+      })
+      .addMeta([{ name: 'apple-mobile-web-app-title', content: m.app_name() }])
       .build();
   },
   shellComponent: RootDocument,
