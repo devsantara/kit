618483d507bd0403d7237b313ef4223de2ff74a9
Edwin Tantawi <edwintantawi.dev@gmail.com>
2026-02-10 11:36:37 +0700

    feat(seo): using @devsantara/head to manage head

--- src/lib/seo/types/alternate.ts
+++
@@ -1,8 +0,0 @@
-export type AlternateLocaleKey<TLocale extends string> =
-  | ('x-default' | TLocale)
-  | (string & {});
-
-export type AlternateLocaleOptions<TLocale extends string> = Record<
-  AlternateLocaleKey<TLocale>,
-  string | URL
->;
